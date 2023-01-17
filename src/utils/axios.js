123bcaa1941ff7be01c33f76b9e0c2fe87ed19c3
Soryu <collages447@gmail.com>
2023-01-17 22:09:20 +0200

    fix baseURL for axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: `https://dnd-squareshnik-server.onrender.com`,
+    baseURL: `https://dnd-squareshnik-server.onrender.com/api`,
 })
 
 instance.interceptors.request.use(config => {
