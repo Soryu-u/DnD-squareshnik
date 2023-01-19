421e0107cc4196954499257c1e031a347578642f
Soryu-u <collages447@gmail.com>
2023-01-19 14:15:47 +0200

    create adaptive header for mobile screen

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,40 +1,78 @@
-import React from 'react'
-import styles from './Header.module.css'
-import { Link } from 'react-router-dom'
-import logo from '../../images/favicon.png'
-import { useDispatch, useSelector } from 'react-redux'
-import { checkIsAuth, logout,  } from '../../redux/features/auth/authSlice'
+import React, { useState } from 'react';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import logo from '../../images/favicon.png';
+import menu from '../../images/menu.png';
+import log_out from '../../images/log-out.png';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkIsAuth, logout } from '../../redux/features/auth/authSlice';
 
-export const Header = ({user}) => {
-  const isAuth = useSelector(checkIsAuth);
-  const dispatch = useDispatch();
+export const Header = ({ user }) => {
+    const [showMenuList, setShowMenuList] = useState(false);
 
-  const logoutHandler = () => {
-    dispatch(logout);
-    window.localStorage.removeItem('token')
-    window.location.reload()
-  }
+    return (
+        <div className={styles.header}>
+            <Link className={styles.logo_bar} to="/">
+                <img className={styles.logo} src={logo} alt="" />
+                <div className={styles.logo_text}>DnD шпаківня</div>
+            </Link>
+            <img
+                className={styles.hamburger}
+                onClick={() => setShowMenuList(!showMenuList)}
+                src={menu}
+                alt=""
+            />
+            {showMenuList && <MenuList user={user} />}
 
-  return (
-    <div className={styles.header}>
-      <Link className={[styles.logoText, styles.logo_bar].join(' ')} to='/'>
-        <img className={styles.logo} src={logo} alt=''/>
-        DnD шпаківня
-      </Link>
-      <div className={styles.right_bar}>
-        {
-          isAuth ? 
-          <>
-          <Link className={styles.link} to='/profile'>{user.username}</Link>
-          <Link className={styles.link} onClick={logoutHandler} to='#'>Вийти</Link>
-          </>
-          :
-          <>
-            <Link className={styles.link} to='/sign_in'>Вхід</Link>
-            <Link className={styles.link} to='/register'>Реєстрація</Link>
-          </> 
-        }
-      </div>
-    </div>
-  )
+            <div className={styles.right_bar}>
+                <UserLogin user={user} />
+            </div>
+        </div>
+    );
+};
+
+function MenuList({ user }) {
+    return (
+        <div className={styles.menu_list}>
+            <UserLogin user={user} />
+        </div>
+    );
+}
+
+function UserLogin({ user }) {
+    const isAuth = useSelector(checkIsAuth);
+    const dispatch = useDispatch();
+
+    const logoutHandler = () => {
+        dispatch(logout);
+        window.localStorage.removeItem('token');
+        window.location.reload();
+    };
+
+    return (
+        <>
+            {isAuth ? (
+                <>
+                    <Link className={styles.link} to="/profile">
+                        {user.username}
+                    </Link>
+                    <img
+                        className={styles.log_out}
+                        onClick={logoutHandler}
+                        src={log_out}
+                        alt="Вийти"
+                    />
+                </>
+            ) : (
+                <>
+                    <Link className={styles.link} to="/sign_in">
+                        Вхід
+                    </Link>
+                    <Link className={styles.link} to="/register">
+                        Реєстрація
+                    </Link>
+                </>
+            )}
+        </>
+    );
 }
